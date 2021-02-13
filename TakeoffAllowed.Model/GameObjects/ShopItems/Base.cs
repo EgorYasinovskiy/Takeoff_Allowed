e39a21ec64bfdd07b6e056fafa8fcc51193c0d74
Egor Yasinovskiy <Yasinovskiy.E@digdes.com>
2021-02-13 03:42:59 +0300

    Add a base goods model. Without implimentation

---
+++ TakeoffAllowed.Model/GameObjects/ShopItems/Base.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace TakeoffAllowed.Model.GameObjects.ShopItems
+{
+	public abstract class Base
+	{
+		public int Price { get; private set; }
+		public string Name { get; private set; }
+		
+		// Maybe some more fields in future
+		public bool CanBuy()
+		{
+			//The method will have to take into account previous purchases and the amount of the player's money. Solution options:
+			//1 - Come up with a model that stores this data and pass it to the method.
+			//2 - Add a delegate to the class, implementing which for each specific model we will issue the corresponding value.
+			throw new NotImplementedException();
+			
+	}
+
+	}
+}
