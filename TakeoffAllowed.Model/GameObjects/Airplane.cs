9cc0f2edc055e33f0ee97ee91309c954befb27c9
Egor Yasinovskiy <Yasinovskiy.E@digdes.com>
2021-02-13 03:21:19 +0300

    Design models

--- TakeoffAllowed.Model/GameObjects/Airplane.cs
+++ TakeoffAllowed.Model/GameObjects/Airplane.cs
@@ -3,10 +3,24 @@ using TakeoffAllowed.Model.Sctructs;
 
 namespace TakeoffAllowed.Model
 {
-	public class Airplane
+	public abstract class Airplane
 	{
 		private double _velocity;
 		private PointF3D _location;
 		private Vector3D _direction;
+		private Guid _guid;
+		
+		public double Velocity => _velocity;
+		public PointF3D Location => _location;
+		public Vector3D Direction => _direction;
+		public Guid Guid => _guid;
+
+		public Airplane(double velocity, PointF3D location, Vector3D direction)
+		{
+			_velocity = velocity;
+			_location = location;
+			_direction = direction;
+			_guid = Guid.NewGuid();
+		}
 	}
 }
---
+++ TakeoffAllowed.Model/GameObjects/PawnAirplane.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using TakeoffAllowed.Model.Sctructs;
+
+namespace TakeoffAllowed.Model.GameObjects
+{
+	class PawnAirplane : Airplane
+	{
+		public PawnAirplane(double velocity, PointF3D location, Vector3D direction) : base(velocity, location, direction)
+		{
+		}
+
+
+
+
+		#region Airplane movement logic
+		// Lower height for landing or to avoid collision.
+		public void Decline()
+		{
+			throw new NotImplementedException("Impliment declining method.");
+		}
+		// Raise height for takeoff, second landing circle or to avoid collisions .
+		public void RaiseUp()
+		{
+			throw new NotImplementedException("Impliment climbing method.");
+		}
+		// Turn aside to avoid collisions.
+		public void TurnAside()
+		{
+			throw new NotImplementedException("Impliment pivot method.");
+		}
+		#endregion
+
+	}
+}
